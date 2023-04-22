62b988885821b7168c1285d055107845ce8d330a
imvickykumar999 <imvickykumar999@gmail.com>
2023-04-22 22:35:21 +0530

    kivy

--- app/python/server.py
+++ app/python/server.py
@@ -1,12 +1,24 @@
-import flask
 
+import kivy
+kivy.require('1.11.1')  # Set to your Kivy version
+from kivy.app import App
+from kivy.uix.textinput import TextInput
+from flask import Flask, request
+from threading import Thread
 
-app = flask.Flask(__name__)
+class MyApp(App):
+    
+    host = '0.0.0.0'
+    port = 5000
 
+    def build(self):
+        self.log_text = TextInput(multiline=True, text='Starting\n')
+        Thread(target=self.start_flask).start()
+        self.log_text.insert_text('Flask listening on %s:%s\n' % (self.host, self.port))
+        return self.log_text
 
-@app.route('/', methods=['GET', 'POST'])
-def handle_request():
-    return "Successful Connection"
+    def start_flask(self):
+        from vixsharefile import run_server
 
-app.run(host="0.0.0.0", port=5000, debug=True)
-
+if __name__ == '__main__':
+    MyApp().run()
